b280099c75e9f34fc8936a7048d924083ec1aa40
Oscar Gonzalez <38501992+Oscar14Lp@users.noreply.github.com>
2024-07-16 19:46:35 -0600

    calculadora de propinas

---
+++ src/components/OrderContents.tsx
@@ -0,0 +1,41 @@
+import { formarCurrency } from "../helpers"
+import { MenuItem, OrderItem } from "../types"
+
+type OrderContentsProps = {
+    order: OrderItem[],
+    removeItem: (id: MenuItem['id']) => void
+}
+
+
+export default function OrderContents({ order, removeItem }: OrderContentsProps) {
+    return (
+        <div>
+            <h2 className=' font-black text-4xl'> Consumo </h2>
+
+            <div className=" space-y-3 mt-10">
+                {order.map(item => (
+                    <div key={item.id}
+                        className="flex justify-between items-center border-t border-sky-900 py-5 last-of-type:border-b"
+                    >
+                        <div>
+                            <p className=" text-lg">
+                                {item.name} - {formarCurrency(item.price)}
+                            </p>
+                            <p className=" font-black">
+                                Cantidad: {item.quantity} - {formarCurrency(item.price * item.quantity)}
+                            </p>
+                        </div>
+
+                        <button className=" bg-red-500 h-8 w-8 rounded-full text-white font-black "
+                            onClick={() => removeItem(item.id)}
+                        >
+                            X
+                        </button>
+
+                    </div>
+                ))
+                }
+            </div>
+        </div>
+    )
+}
---
+++ src/components/OrderTotal.tsx
@@ -0,0 +1,47 @@
+import { useMemo } from "react"
+import { OrderItem } from "../types"
+import { formarCurrency } from "../helpers"
+
+type OrderTotalProps = {
+    order: OrderItem[],
+    tip: number,
+    placeOrder: () => void
+}
+
+export default function OrderTotal({ order, tip, placeOrder }: OrderTotalProps) {
+
+
+    const subTotalAmount = useMemo(() => order.reduce((total, item) => total + (item.quantity * item.price), 0), [order])
+
+    const tipAmount = useMemo(() => subTotalAmount * tip, [tip, order])
+
+    const totalAmount = useMemo(() => subTotalAmount + tipAmount, [tip, order])
+
+
+    return (
+        <>
+            <div className=" space-y-3 ">
+                <h2 className=" font-black text-2xl"> Total y Propina: </h2>
+                <p>Subtotal a pagar: {''}
+                    <span className=" font-bold">{formarCurrency(subTotalAmount)}</span>
+                </p>
+                <p>Propina: {''}
+                    <span className=" font-bold">{formarCurrency(tipAmount)}</span>
+                </p>
+                <p>Total a pagar: {''}
+                    <span className=" font-bold">{formarCurrency(totalAmount)}</span>
+                </p>
+
+            </div>
+
+            <button
+                className=" w-full bg-black p-3 uppercase text-white font-bold mt-10 disabled:opacity-10"
+                disabled={totalAmount === 0}
+                onClick={placeOrder}
+            >
+                Guardar Orden
+            </button>
+
+        </>
+    )
+}
